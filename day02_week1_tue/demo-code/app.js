50d835c8ad3761fd44e709ede5d348a666ca0def
a-she <al@alswebdesigns.net>
2016-05-10 07:13:09 -0700

    demo for day 2 lec/lab

---
+++ day02_week1_tue/demo-code/app.js
@@ -0,0 +1,25 @@
+// 201
+// 3-tue-lecture code demo
+
+var userName = prompt('What is your name?');
+console.log('The user said there name is ' + userName);
+
+alert('Hey there ' + userName + ', I\'m going to ask you some questions about me. Please click "ok", then answer Y or N to the next question.');
+
+var keepAsking = true;
+
+while (keepAsking) {
+  var userResponse = prompt('Was, I born on the moon?');
+  console.log('userResponse: ' + userResponse);
+
+  if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
+    alert('Nope, I was actualy born on the moon. I\'ma moonian computer language.');
+    keepAsking = false;
+  } else if (userResponse === 'Y') { // TODO: Make this more robust!
+    alert('Nice, I was born on the moon');
+    keepAsking = false;
+  } else {
+    alert('Ooops!, you should have given a Y or N, so you loose this round.');
+  }
+
+} // while
