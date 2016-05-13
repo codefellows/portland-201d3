a2f06294ba75837510db6699ebfa198e6126b331
a-she <al@alswebdesigns.net>
2016-05-13 10:55:59 -0700

    inital

---
+++ day05_week1_fri/demo_test/test.js
@@ -0,0 +1,19 @@
+var numTests = 0;
+var numCorrect = 0;
+
+if (typeof sum != 'undefined') {
+  var y = sum(5, 6);
+  if (11 == y) {
+    console.log('PASS!');
+    numCorrect++;
+  }
+  else {
+    console.log('fail. Wrong answer');
+  }
+}
+else {
+  console.log('fail. No such function');
+}
+
+numTests++;
+console.log('You got ' + numCorrect + ' out of ' + numTests++);
