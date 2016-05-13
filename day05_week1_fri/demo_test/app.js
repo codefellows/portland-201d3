6b162edfbefa928600f8b1e3f5abd8ca02ff64c6
a-she <al@alswebdesigns.net>
2016-05-13 11:20:24 -0700

    Fixed script loading order

---
+++ day05_week1_fri/demo_test/app.js
@@ -0,0 +1,5 @@
+// DOM Aware JS file
+
+q1_answer = document.getElementById('q1');
+
+q1_answer.textContent = 'The sum of 5 and 6 is ' + sum(5, 6);
--- day05_week1_fri/demo_test/test.js
+++ day05_week1_fri/demo_test/test.js
@@ -3,7 +3,7 @@ var numCorrect = 0;
 
 if (typeof sum != 'undefined') {
   var y = sum(5, 6);
-  if (11 == y) {
+  if (11 === y) {
     console.log('PASS!');
     numCorrect++;
   }
@@ -16,4 +16,21 @@ else {
 }
 
 numTests++;
-console.log('You got ' + numCorrect + ' out of ' + numTests++);
+console.log('You got ' + numCorrect + ' out of ' + numTests);
+
+if (typeof sum != 'undefined') {
+  var y = sum(-10, 10);
+  if (0 === y) {
+    console.log('PASS!');
+    numCorrect++;
+  }
+  else {
+    console.log('fail. Wrong answer');
+  }
+}
+else {
+  console.log('fail. No such function');
+}
+
+numTests++;
+console.log('You got ' + numCorrect + ' out of ' + numTests);
